5eff171516d6141a8ae01af3f106aaf3106672d9
BanDroid <dbbhuh5@gmail.com>
2024-07-24 11:36:32 +0000

    demo: add ocean theme for demo

--- src/app/toggle-mode.tsx
+++ src/app/toggle-mode.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
@@ -9,32 +10,44 @@ import { useCustomTheme } from "@/config/theme";
 export default function ToggleMode() {
   const { toggleColorMode, currentMode, currentTheme } = useCustomTheme();
   return (
-    <Box
-      sx={{
-        display: "flex",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        bgcolor: "background.default",
-        color: "text.primary",
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {currentMode /* != undefined && currentMode */} mode in {currentTheme}{" "}
-      theme
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={() =>
-          toggleColorMode(
-            currentTheme !== "light" ? "light" : "dark",
-            currentMode !== "light" ? "light" : "dark"
-          )
-        }
-        color="inherit"
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "background.default",
+          color: "text.primary",
+          // borderRadius: 1,
+          p: 3,
+        }}
       >
-        {currentMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
+        {currentMode /* != undefined && currentMode */} mode in {currentTheme}{" "}
+        theme
+        <IconButton
+          sx={{ ml: 1 }}
+          onClick={() =>
+            toggleColorMode(
+              currentTheme !== "light" ? "light" : "dark",
+              currentTheme !== "light" ? "light" : "dark"
+            )
+          }
+          color="inherit"
+        >
+          {currentMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      </Box>
+      <Button
+        sx={{
+          mb: 2,
+        }}
+        onClick={() => toggleColorMode("ocean", "light")}
+        variant="contained"
+        color="primary"
+      >
+        Toggle Ocean Theme
+      </Button>
+    </>
   );
 }
